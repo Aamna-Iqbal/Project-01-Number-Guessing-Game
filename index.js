29ec1a5d26a0c886a2070357eb31383f53c04c19
Aamna-Iqbal <amnaiqbal834@gmail.com>
2024-06-04 13:47:29 +0500

    Adding Numberle Number Guessing Game

---
+++ index.js
@@ -0,0 +1,16 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 20 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Enter your guess Number (Number list from 1 to 20)"
+    }
+]);
+if (answer.userGuessNumber === randomNumber) {
+    console.log("congratulation ! You guess a correct number.");
+}
+else {
+    console.log("Sorry, you guess a wrong number");
+}
